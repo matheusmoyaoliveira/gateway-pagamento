4fc1dc55430b732097b2baeb471927b2a3b3e8e3
matheusmoyaoliveira <matheus.dev.moya@hotmail.com>
2026-03-07 13:10:36 -0300

    feat(webhooks): dispatch pending events via WebClient

--- src/main/java/com/project/gateway_pagamento/GatewayPagamentoApplication.java
+++ src/main/java/com/project/gateway_pagamento/GatewayPagamentoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class GatewayPagamentoApplication {
 
-	@EnableScheduling
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayPagamentoApplication.class, args);
 	}
